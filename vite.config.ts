d5bad8a93038e2177702ec62339f63e4b8373da3
shaikazeeza7386-dot <shaikazeeza7386@gmail.com>
2026-02-23 19:15:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+base: "/waste-not-buzz/",
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -19,9 +19,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
